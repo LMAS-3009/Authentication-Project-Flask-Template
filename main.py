19491d4822761cd19948e7547a9f75ab9a9a63d0
unknown <30lmas09@gmail.com>
2021-03-25 14:04:23 +0530

    project template

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return redirect('/login')
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
